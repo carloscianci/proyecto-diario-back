0e5e844f126f10b5568c5d0bb14079720df98269
Carlos Cianci <carloscianci@gmail.com>
2022-06-13 23:06:19 -0300

    Funcionalidad principal de las noticias

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const app = express()
+require('./database/db')
 
 const port = 8000
 
 app.use(express.json())
 app.use(express.urlencoded())
 
-app.get('/', (req, res ) => {
-    res.json({
-        message: "Estas en el home."
-    })
-})
+const noticia = require('./routes/noticias')
+
+app.use('/noticia', noticia)
+
 
 app.listen(port,() => {
     console.log("Servidor corriendo en puerto: " + port)

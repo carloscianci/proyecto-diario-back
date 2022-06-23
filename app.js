80273e08fceb5a1544f964131c2d91ea9ccee203
Carlos Cianci <carloscianci@gmail.com>
2022-06-22 22:29:29 -0300

    Implemento cors para permitir el consumo de dominios cruzados desde react

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 require('./database/db')
 
 const port = 8000
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded())
 

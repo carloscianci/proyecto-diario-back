3e72cc243f189d9349ea3c0329aca418e4ba220b
Carlos Cianci <carloscianci@gmail.com>
2022-07-03 20:16:42 -0300

    Arreglos al proyecto para deploy en heroku

--- app.js
+++ app.js
@@ -1,24 +1,49 @@
-const express = require('express')
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const cors = require('cors')
-const app = express()
 require('./database/db')
-
-const port = 8000
-
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded())
 
 const index = require('./routes/index')
 const noticia = require('./routes/noticias')
 const suscripcion = require('./routes/suscripcion')
 const usuario = require('./routes/usuario')
 
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
+
 app.use('/', index)
 app.use('/usuario', usuario) 
 app.use('/noticia', noticia)
 app.use('/suscripcion', suscripcion)
 
-app.listen(port,() => {
-    console.log("Servidor corriendo en puerto: " + port)
-})
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+
